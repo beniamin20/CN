6f3b0d83530b43d556820c5bff75832b58c3d7e1
Victor Todoran <victor.todoran@evozon.com>
2019-05-20 10:57:34 +0300

    Refactor netwon

--- L12/newton.m
+++ L12/newton.m
@@ -1,8 +1,6 @@
-function result = newton(f,x1,err,nmax)
-  ff = f(x);  
-  ffd = diff(ff, x)  
-  
-  for i=1:n
+function result = newton(f,ffd,x1,err,nmax)
+
+  for i=1:nmax
     result = x1 - (1/ffd(x1)) * f(x1);
     if abs(result - x1) < err
        return;
